9f367b6aadd73d2bacab82b9e83c1f994430ebee
charlesdobbs02 <charlesdobbs02@yahoo.com>
2025-06-21 11:58:50 -0700

    Add files via upload

---
+++ turnbasedrpg/Scripts/character.gd
@@ -0,0 +1,68 @@
+class_name Character
+
+extends Node2D
+
+signal OnTakeDamage (health : int)
+signal OnHeal (health : int)
+
+@export var is_player : bool
+@export var cur_health : int
+@export var max_health : int
+
+@export var combat_actions : Array[CombatAction]
+
+var target_scale : float = 1.0
+
+@onready var audio : AudioStreamPlayer = $AudioStreamPlayer
+
+var take_damage_sfx : AudioStream = preload("res://Audio/take_damage.wav")
+var heal_sfx : AudioStream = preload("res://Audio/heal.wav")
+
+@export var facing_left : bool = false
+@export var display_texture : Texture2D
+
+@onready var sprite : Sprite2D = $Sprite
+
+func _ready () -> void:
+	sprite.flip_h = facing_left
+	sprite.texture = display_texture
+
+func begin_turn ():
+	target_scale = 1.1
+	
+	if is_player:
+		print("Player turn has begun")
+	else:
+		print("AI turn has begun")
+
+func end_turn ():
+	target_scale = 0.9
+
+func _process (delta: float) -> void:
+	scale.x = lerp(scale.x, target_scale, delta * 10)
+	scale.y = lerp(scale.y, target_scale, delta * 10)
+
+func take_damage (amount : int):
+	cur_health -= amount
+	OnTakeDamage.emit(cur_health)
+	_play_audio(take_damage_sfx)
+
+func heal (amount : int):
+	cur_health += amount
+	cur_health = clamp(cur_health, 0, max_health)
+	OnHeal.emit(cur_health)
+	_play_audio(heal_sfx)
+
+func cast_combat_action (action : CombatAction, opponent : Character):
+	if action == null:
+		return
+	
+	if action.melee_damage > 0:
+		opponent.take_damage(action.melee_damage)
+	
+	if action.heal_amount > 0:
+		heal(action.heal_amount)
+
+func _play_audio (stream : AudioStream):
+	audio.stream = stream
+	audio.play()
---
+++ turnbasedrpg/Scripts/game_manager.gd
@@ -0,0 +1,106 @@
+extends Node2D
+
+@export var player_character : Character
+@export var ai_character : Character
+var current_character : Character
+
+@onready var player_ui = $CanvasLayer/CombatActionsUI
+
+var game_over : bool = false
+@onready var end_screen = $CanvasLayer/EndScreen
+
+func _ready() -> void:
+	player_character.OnTakeDamage.connect(_on_player_take_damage)
+	ai_character.OnTakeDamage.connect(_on_ai_take_damage)
+	
+	end_screen.visible = false
+	
+	next_turn()
+
+func _on_player_take_damage (health : int):
+	if health <= 0:
+		end_game(ai_character)
+
+func _on_ai_take_damage (health : int):
+	if health <= 0:
+		end_game(player_character)
+
+func end_game (winner : Character):
+	game_over = true
+	end_screen.visible = true
+	
+	if winner == player_character:
+		end_screen.set_header_text("You have won!")
+	else:
+		end_screen.set_header_text("You have been defeated!")
+
+func next_turn ():
+	if game_over:
+		return
+	if current_character != null:
+		current_character.end_turn()
+	if current_character == ai_character or current_character == null:
+		current_character = player_character
+	else:
+		current_character = ai_character
+	
+	current_character.begin_turn()
+	
+	if current_character.is_player:
+		player_ui.visible = true
+		player_ui.set_combat_actions(player_character.combat_actions)
+	else:
+		player_ui.visible = false
+		var wait_time = randf_range(0.5, 1.5)
+		await get_tree().create_timer(wait_time).timeout
+		
+		var action_to_cast = ai_decide_combat_action()
+		ai_character.cast_combat_action(action_to_cast, player_character)
+		
+		await get_tree().create_timer(0.5).timeout
+		next_turn()
+
+func player_cast_combat_action (action : CombatAction):
+	if player_character != current_character:
+		return
+	
+	player_character.cast_combat_action(action, ai_character)
+	player_ui.visible = false
+	await get_tree().create_timer(0.5).timeout
+	next_turn()
+
+func ai_decide_combat_action () -> CombatAction:
+	if ai_character != current_character:
+		return null
+	
+	var ai = ai_character
+	var player = player_character
+	
+	var actions = ai.combat_actions
+	
+	var weights : Array[int] = []
+	var total_weight = 0
+	
+	var ai_health_perc = float(ai.cur_health) / float(ai.max_health)
+	var player_health_perc = float(player.cur_health) / float(player.max_health)
+	
+	for action in actions:
+		var weight : int = action.base_weight
+		
+		if player.cur_health <= action.melee_damage:
+			weight *= 3
+		if action.heal_amount > 0:
+			weight *= 1 + (1 - ai_health_perc)
+		
+		weights.append(weight)
+		total_weight += weight
+	
+	var cumulative_weight = 0
+	var rand_weight = randi_range(0, total_weight)
+	
+	for i in len(actions):
+		cumulative_weight += weights[i]
+		
+		if rand_weight < cumulative_weight:
+			return actions[i]
+	return null
